46674dcfd6d9cb31d9231439dc3c84c28dca9a0f
Marcel Hofmann <hofmann.marcel@hm.edu>
2020-10-20 21:59:31 +0200

    First App Main Activity zweiter commit

--- FirstProject/app/src/main/java/com/example/firstproject/MainActivity.java
+++ FirstProject/app/src/main/java/com/example/firstproject/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.firstproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button app1 = (Button)findViewById(R.id.App1);
+        Button app2 = (Button)findViewById(R.id.App2);
+        Button app3 = (Button)findViewById(R.id.App3);
+        Button app4 = (Button)findViewById(R.id.App4);
+
+        app2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent startIntent = new Intent(getApplicationContext(), ActivityApp2.class);
+                startActivity(startIntent);
+            }
+        });
+
+        app1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent startIntent = new Intent(getApplicationContext(), ActivityApp1.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
